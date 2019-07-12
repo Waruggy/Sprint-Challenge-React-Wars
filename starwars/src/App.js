8bb8339d35d1c22e20e50d9dac53085384fc0073
Charles <cwang8413@yahoo.com>
2019-07-12 16:46:21 -0700

    Here is the test

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Header } from 'semantic-ui-react';
 
 const App = () => {
-  const[star,setStar] = useState({});
-
+  const[star, setStar] = useState([]);
+    
+    
   useEffect(() => {
-      axios.get('https://swapi.co/api/people/')
+      axios.get("https://swapi.co/api/people/")
         .then (res => {
           console.log(res)
-          setStar(res.data)
+          setStar(res.data.results)
       })
       console.log ('Character', star);
+      
   }, []);
  
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
-       
+        <h1 className="ui header">
+        <Header>React Wars</Header>
+        </h1>
+        {star.map(character => (<p>{character.name}</p>))}
       </div>
     );
   
