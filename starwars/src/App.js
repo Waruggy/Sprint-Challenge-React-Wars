a77531ff79db64262bc5e211514dd06a6d5cfe59
Charles <cwang8413@yahoo.com>
2019-07-12 10:48:32 -0700

    File is a bit messed up.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,41 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
-    };
-  }
+const App = () => {
+  const[star,setStar] = useState({});
 
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
-  }
-
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
+  useEffect(() => {
+      axios.get('https://swapi.co/api/people/')
+        .then (res => {
+          console.log(res)
+          setStar(res.data)
       })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  };
-
-  render() {
+      console.log ('Character', star);
+  }, []);
+ 
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+       
       </div>
     );
-  }
+  
 }
 
 export default App;
